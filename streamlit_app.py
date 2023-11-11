f96307871d5754c2d7612727ae0e2cd5ddf9d64e
gurditsingh09bedi <148595921+gurditsingh09bedi@users.noreply.github.com>
2023-11-11 13:28:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import os
-import warnings
 
-warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title="Ukraine War",layout="wide")
 
